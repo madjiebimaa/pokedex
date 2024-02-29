dfbf4a198af32cf33e3df5dce098672451632c3f
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-02-29 16:14:05 +0700

    feat: base components by Shadcn UI

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorColor?: string;
+}
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  ProgressProps
+>(({ className, value, indicatorColor, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-gray-100 dark:bg-gray-800",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-gray-900 transition-all dark:bg-gray-50"
+      style={{
+        transform: `translateX(-${100 - (value || 0)}%)`,
+        backgroundColor: indicatorColor,
+      }}
+    />
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
